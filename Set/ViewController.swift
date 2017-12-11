9247018eda0af149e5d25343bd2000e09c3efe7b
kvcruzat <kvcruzat@gmail.com>
2017-12-11 13:53:27 +0000

    Added outlets for UI elements

--- Set/ViewController.swift
+++ Set/ViewController.swift
@@ -9,7 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+    }
 
+    @IBAction func newGame(_ sender: UIButton) {
+    }
+    
+    @IBAction func dealCards(_ sender: UIButton) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
