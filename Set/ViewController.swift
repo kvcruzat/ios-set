03c0dea958c6fd8e6e30892f63ab13119acd3f92
kvcruzat <kvcruzat@gmail.com>
2017-12-12 16:44:16 +0000

    Added shadings, and colour attributes to shapes. Randomly shows 12 cards at the start and deal more cards functionality implemented

--- Set/ViewController.swift
+++ Set/ViewController.swift
@@ -13,26 +13,53 @@ class ViewController: UIViewController {
     lazy var game = Set()
     
     let shapes = ["▲", "●", "■"]
-    let colours = [UIColor.red, UIColor.orange, UIColor.black]
+    let colours = [UIColor.red, UIColor.green, UIColor.blue]
     let shadings = ["filled", "outline", "striped"]
+    
+    var cardIndex = [Int:Card]()
+    var faceDownIndex = [Int]()
     
     @IBOutlet var cardButtons: [UIButton]!{
         didSet {
-            for index in cardButtons.indices {
+            var cardArray: [Int] = Array(0...cardButtons.count-1)
+            
+            var last = cardArray.count - 1
+
+            while last > 0 {
+                let rand = Int(arc4random_uniform(UInt32(last)))
+                cardArray.swapAt(last, rand)
+                last -= 1
+            }
+            
+            while game.faceUpCards.count < 12{
+                let index = cardArray.removeFirst()
+            
                 let card = game.deckCards.removeFirst()
                 game.faceUpCards.append(card)
                 
                 let button = cardButtons[index]
-                let identifier = game.faceUpCards[index].identifier
-                let buttonTitle = String(repeating: shapes[identifier["shape"]!], count: identifier["amount"]!)
-                button.setTitle(buttonTitle, for: UIControlState.normal)
+                let identifier = card.identifier
+                let attribText = buildAttributes(identifier: identifier)
+                button.setAttributedTitle(attribText, for: UIControlState.normal)
+                
+                cardIndex[index] = card
             }
+            for index in cardArray {
+                let button = cardButtons[index]
+                let layer = CAShapeLayer()
+                layer.frame = .zero
+                button.layer.mask = layer
+                button.isEnabled = false
+            }
+            faceDownIndex = cardArray
         }
     }
     
     @IBAction func touchCard(_ sender: UIButton) {
         if let cardNumber = cardButtons.index(of: sender){
-            let card = game.faceUpCards[cardNumber]
+            print("card number \(cardNumber)")
+            let card = cardIndex[cardNumber]!
+            print("\(card.identifier["colour"]!), \(shadings[card.identifier["shading"]!])")
             if game.selectedCards.contains(card){
                 deselectCard(index: cardNumber)
                 game.selectedCards.remove(at: game.selectedCards.index(of: card)!)
@@ -47,6 +74,21 @@ class ViewController: UIViewController {
     }
     
     @IBAction func dealCards(_ sender: UIButton) {
+        if faceDownIndex.count > 0 {
+            for _ in 1...3 {
+                let index = faceDownIndex.removeFirst()
+                let card = game.deckCards.removeFirst()
+                game.faceUpCards.append(card)
+                cardIndex[index] = card
+                
+                let button = cardButtons[index]
+                let identifier = card.identifier
+                let attribText = buildAttributes(identifier: identifier)
+                button.setAttributedTitle(attribText, for: UIControlState.normal)
+                button.isEnabled = true
+                button.layer.mask = nil
+            }
+        }
     }
     
     private func selectCard(index: Int){
@@ -63,6 +105,38 @@ class ViewController: UIViewController {
         button.layer.cornerRadius = 1.0
     }
     
+    private func buildAttributes(identifier: [String:Int]) -> NSAttributedString{
+        var attributes: [NSAttributedStringKey : Any] = [
+            .strokeColor : colours[identifier["colour"]!],
+            .strokeWidth : -5.0
+        ]
+        
+        switch shadings[identifier["shading"]!] {
+        case "filled":
+            attributes[.foregroundColor] = colours[identifier["colour"]!].withAlphaComponent(1.0)
+            attributes[.strokeWidth] = -5.0
+        case "striped":
+            attributes[.foregroundColor] = colours[identifier["colour"]!].withAlphaComponent(0.20)
+        case "outline":
+            attributes[.strokeWidth] = 5.0
+        default:
+            print("Error shading not found")
+        }
+        
+        let buttonTitle = String(repeating: shapes[identifier["shape"]!], count: identifier["amount"]!)
+        return NSAttributedString(string: buttonTitle, attributes: attributes)
+    }
 
 }
 
+extension Int {
+    var arc4random: Int {
+        if self > 0 {
+            return Int(arc4random_uniform(UInt32(self)))
+        } else if self < 0 {
+            return -Int(arc4random_uniform(UInt32(abs(self))))
+        } else {
+            return 0
+        }
+    }
+}
